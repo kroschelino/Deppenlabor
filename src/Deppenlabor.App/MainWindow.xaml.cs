36b9978dcae679afb010707ff3011cf926c4738f
SSC-OS\skroschel <skroschel@slashwhy.de>
2023-05-10 12:34:31 +0200

    Revise main layout

--- src/Deppenlabor.App/MainWindow.xaml.cs
+++ src/Deppenlabor.App/MainWindow.xaml.cs
@@ -1,20 +1,16 @@
+using Windows.ApplicationModel;
 using Microsoft.UI.Xaml;
 
-namespace Deppenlabor
-{
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            this.InitializeComponent();
-        }
+namespace Deppenlabor;
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        ExtendsContentIntoTitleBar = true;
+        AppTitleTextBlock.Text = AppInfo.Current.DisplayInfo.DisplayName;
+        SetTitleBar(AppTitleBar);
     }
 }
